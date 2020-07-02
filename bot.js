6e0d394b8e755986b3ff99f0a7ea94cce6a61656
DarkZYT <aouabsss@gmail.com>
2020-07-02 13:48:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,19 @@ let request = require('request');
 
 var welcome_gif="https://media1.tenor.com/images/9f75a735e474d9b8c127fd5383717f44/tenor.gif?itemid=13849146"
 
+
+//Commands
 client.on('message',message=>{
 
-	//Now let's get to real work lol
+	if(message.channel.id!="728025726556569631") return;
+	var args=message.content.toLowerCase().split(" ");
+	
+	if(args[0]=="ping"){
+		message.channel.send("**Pong!** My ping is *"+(client.ping|0)+" ms*!);
+	} else if(args[0]=="level"){
+		message.channel.send("File saving isn't working yet so shut the fuck up");
+	}
+	
 	
 });
 
@@ -15,7 +25,7 @@ client.on('message',message=>{
 client.on('guildMemberAdd',member=>{
 
 	var welcome_channel=member.guild.channels.find("id","728008557911605340");
-	welcome_channel.send(new Discord.MessageEmbed().setTitle("Welcome " + member.displayName + "!").setImage(welcome_gif).setThumbnail(member.user.displayAvatarURL()));
+	welcome_channel.send(new Discord.MessageEmbed().addField("Hey hey hey!","We've been waiting for you!").setTitle("Welcome " + member.displayName + "!").setThumbnail(member.user.displayAvatarURL()));
 	
 });
 
