3e715524dd3266fd6e142eb8a5c6ef6420b0ce06
DarkZYT <aouabsss@gmail.com>
2020-07-02 01:46:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,19 @@ let snekfetch = require("snekfetch");
 const client = new Discord.Client();
 let request = require('request');
 
+var welcome_gif="https://media1.tenor.com/images/9f75a735e474d9b8c127fd5383717f44/tenor.gif?itemid=13849146"
+
 client.on('message',message=>{
 
-	if(message.content=="who is the best?"){
-		message.channel.send("Aouab! (and anxxie ofc)");	
-	}
+	//Now let's get to real work lol
+	
+});
+
+///Welcoming
+client.on('guildMemberAdd',member=>{
+
+	var welcome_channel==member.guild.channels.find("id","728008557911605340");
+	welcome_channel.send(new Discord.RichEmbed().setTitle("Welcome " + member.displayName + "!").setImage(welcome_gif).setThumbnail(member.user.defaultAvatarURL));
 	
 });
 
