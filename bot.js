8b8034225b5f7675a64e9b6667c1ba5d0b2425e6
DarkZYT <aouabsss@gmail.com>
2020-07-02 13:52:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
-let snekfetch = require("snekfetch");
 const client = new Discord.Client();
-let request = require('request');
 
 var welcome_gif="https://media1.tenor.com/images/9f75a735e474d9b8c127fd5383717f44/tenor.gif?itemid=13849146"
 
