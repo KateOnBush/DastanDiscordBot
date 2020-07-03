9958dbeab5a2c403b91f9882a108f2bb6303833d
DarkZYT <aouabsss@gmail.com>
2020-07-03 03:47:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,27 +20,26 @@ var info = {
 		}
 	},
 	load: function(id){
-		this.exists=false;
-		console.log(client.channels.cache.get("728363315138658334").messages.fetch().then(col=>{
+		exists=false;
+		client.channels.cache.get("728363315138658334").messages.fetch().then(col=>{
 			messages=col.toJSON();
 			for(var i=0;i<messages.length;i++){
 				if(messages[i].content.includes(id)){
-					info.exists=JSON.parse(messages[i].content);
+					exists=JSON.parse(messages[i].content);
 				} 
 			}
-			if(info.exists==false){
+			if(exists==false){
 				//Default data
 				var def={
 					id: id,
 					level: 1,
 					last_message: 0
 				}
-				info.init(def);
-				info.exists=def;
+				this.init(def);
+				exists=def;
 			}
-			return "hey";
-		}));
-		return this.exists;
+		});
+		return exists;
 	}
 }
 
