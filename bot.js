d3df5a659e6a4164490631a5bc38c52659f58f6e
DarkZYT <aouabsss@gmail.com>
2020-07-07 21:54:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -578,7 +578,7 @@ client.on('guildMemberRemove',member=>{
 //
 // -------------------------------------------
 
-const music = require("discord.js-musicbot-v2");
+const music = require("dqq-music");
 music.start(client,{
 	youtubeKey: "AIzaSyA2WXr8bHDvK2bD0Llt9qeA29wtwR13HBI",
 	botPrefix: "music ",
