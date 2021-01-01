62694bd9f9f1a9a05c86d3367c4b8bdd7fa1b705
DarkZYT <aouabsss@gmail.com>
2021-01-02 00:22:34 +0100

    Update music.js

--- modules/music.js
+++ modules/music.js
@@ -1,4 +1,19 @@
 const ytdl = require("ytdl-core");
 const Discord = require("discord.js");
 
-module.exports = async function(){}
+function array_from(array, from, join){ 
+  return array.filter((t,i)=>i>=from);
+}
+
+module.exports = async function(client, message, args){
+
+  if(args[0]=="play"){
+     if(args[1]){
+        let inf = await ytdl.getInfo(array_from(args,1).join(" "));
+        message.channel.send("You looked for: " + inf.videoDetails.title + " in " + inf.videoDetails.video_url);
+     }
+  } else {
+    
+  }
+  
+}
